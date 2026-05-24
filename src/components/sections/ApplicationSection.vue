2074fd4b515fed6c75733bf662aaef302da85218
leno\LENOVO <esperanzapolo999@gmail.com>
2026-05-24 17:13:23 +0200

    html de la sección de Aplicaciones

---
+++ src/components/sections/ApplicationSection.vue
@@ -0,0 +1,73 @@
+<template>
+  <section id="aplicaciones" ref="applicationSection" class="application-section">
+    <div class="application-phones">
+      <figure data-application-reveal>
+        <img :src="applications.phoneFlat" alt="Aplicación de Bandcamp en móvil" />
+      </figure>
+      <figure data-application-reveal>
+        <img :src="applications.instagramHand" alt="Perfil de Bandcamp en móvil" />
+      </figure>
+    </div>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.frame" alt="Poster Bandcamp en interior" />
+    </figure>
+
+    <div class="application-posters">
+      <figure data-application-reveal>
+        <img :src="applications.posterBlack" alt="Poster negro Bandcamp" />
+      </figure>
+      <figure data-application-reveal>
+        <img :src="applications.posterWhite" alt="Poster blanco Bandcamp" />
+      </figure>
+    </div>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.tickets" alt="Tickets Bandcamp" />
+    </figure>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.tshirt" alt="Camiseta y stickers Bandcamp" />
+    </figure>
+
+    <figure class="application-full application-box" data-application-reveal>
+      <img :src="applications.box" alt="Caja Bandcamp" />
+    </figure>
+  </section>
+</template><template>
+  <section id="aplicaciones" ref="applicationSection" class="application-section">
+    <div class="application-phones">
+      <figure data-application-reveal>
+        <img :src="applications.phoneFlat" alt="Aplicación de Bandcamp en móvil" />
+      </figure>
+      <figure data-application-reveal>
+        <img :src="applications.instagramHand" alt="Perfil de Bandcamp en móvil" />
+      </figure>
+    </div>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.frame" alt="Poster Bandcamp en interior" />
+    </figure>
+
+    <div class="application-posters">
+      <figure data-application-reveal>
+        <img :src="applications.posterBlack" alt="Poster negro Bandcamp" />
+      </figure>
+      <figure data-application-reveal>
+        <img :src="applications.posterWhite" alt="Poster blanco Bandcamp" />
+      </figure>
+    </div>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.tickets" alt="Tickets Bandcamp" />
+    </figure>
+
+    <figure class="application-full" data-application-reveal>
+      <img :src="applications.tshirt" alt="Camiseta y stickers Bandcamp" />
+    </figure>
+
+    <figure class="application-full application-box" data-application-reveal>
+      <img :src="applications.box" alt="Caja Bandcamp" />
+    </figure>
+  </section>
+</template>
